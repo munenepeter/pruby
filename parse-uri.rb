32d7afbc25a2c0125e61bc5cb341e3540d4b47be
munenepeter <peternjega7@gmail.com>
2022-05-27 17:15:35 +0300

    printed the actal output

--- parse-uri.rb
+++ parse-uri.rb
@@ -1,6 +1,6 @@
 require "uri"
 
 uri = URI.parse("http://localhost:3000")
-uri.scheme # http
-uri.host # localhost
-uri.port # 3000
+puts uri.scheme + "\n" # http
+puts uri.host + "\n"  # localhost
+puts uri.port + "\n"  # 3000
